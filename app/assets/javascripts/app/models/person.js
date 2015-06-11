b4435636311deac6eca24570313176ce472702b5
Flaburgan <flaburgan@geexxx.fr>
2015-06-11 14:32:16 +0200

    x

--- app/assets/javascripts/app/models/person.js
+++ app/assets/javascripts/app/models/person.js
@@ -6,8 +6,9 @@ app.models.Person = Backbone.Model.extend({
   },
 
   initialize: function() {
-    if( this.get('profile') )
+    if (this.get('profile')) {
       this.profile = new app.models.Profile(this.get('profile'));
+    }
   },
 
   isSharing: function() {
@@ -31,20 +32,22 @@ app.models.Person = Backbone.Model.extend({
     var block = new app.models.Block({block: {person_id: this.id}});
 
     // return the jqXHR with Promise interface
-    return block.save()
-      .done(function() { app.events.trigger('person:block:'+self.id); });
+    return block.save().done(function() {
+      app.events.trigger('person:block:' + self.id);
+    });
   },
 
   unblock: function() {
     var self = this;
-    if( !this.get('block') ) {
+    if (!this.get('block')) {
       var def = $.Deferred();
       return def.reject();
     }
 
     var block = new app.models.Block({id: this.get('block').id});
-    return block.destroy()
-      .done(function() { app.events.trigger('person:unblock:'+self.id); });
+    return block.destroy().done(function() {
+      app.events.trigger('person:unblock:'+self.id);
+    });
   }
 });
 // @license-end
